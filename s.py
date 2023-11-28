fb1e984c03466384597248beb65c6b1817a4daea
123well <152323637+123well@users.noreply.github.com>
2023-11-28 21:45:34 +0300

    Add files via upload

---
+++ s.py
@@ -0,0 +1,103 @@
+# циклы
+
+# первая задача
+'''
+a = int(input())
+c = 0
+d = 0
+for x in range (0, a):
+    b = int(input())
+    if b == 10:
+        c += 1
+    if b < 5:
+        d += 1
+if c > 0:
+    c = "YES"
+else:
+    c = "NO"
+print(d, c, sep = "\n")'''
+
+# вторая задача
+'''
+a = 1
+b = 0
+while a != 0:
+    a = int(input())
+    if a % 10 == 0 and a != 0:
+        b += 1
+print(b)'''
+
+# ветвление
+
+# наименьшее из двух чисел
+'''
+a = int(input())
+b = int(input())
+if a > b:
+    print(b)
+else:
+    print(a)'''
+
+# только
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+d = 0
+if a >= 0:
+    d += a
+if b >= 0:
+    d += b
+if c >= 0:
+    d += c
+print(d)'''
+
+# принадлежность 1
+'''
+a = int(input())
+if a > -1 and a < 17:
+    print("Принадлежит")
+else:
+    print("Не принадлежит")'''
+
+# принадлежность 3
+'''
+a = int(input())
+if (a > -30 and a <= -2) or (a > 7 and a <= 25):
+    print("Принадлежит")
+else:
+    print("Не принадлежит")'''
+
+# ход ладьи
+'''
+a = int(input())
+b = int(input())
+a1 = int(input())
+b1 = int(input())
+s = 0
+if a != a1:
+    s += 1
+if b != b1:
+    s += 1
+if s >= 2:
+    print("NO")
+else:
+    print("YES")'''
+
+# количество дней
+'''
+a = int(input())
+if a >= 10:
+    if a % 2 == 0:
+        print(31)
+    else:
+        print(30)
+elif a >= 3 and a <= 9:
+    if a % 2 == 1:
+        print(31)
+    else:
+        print(30)
+elif a == 1:
+    print(31)
+else:
+    print(28)'''
